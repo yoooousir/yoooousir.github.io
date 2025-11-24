3d5b771c33982ff1e99458311b22d95cec596184
김채영 <gimchaeyeong53@gmail.com>
2025-11-24 14:59:40 +0900

    Update language-switcher.js

--- assets/js/language-switcher.js
+++ assets/js/language-switcher.js
@@ -119,7 +119,7 @@ TxtRotate.prototype.tick = function() {
   this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
   var that = this;
-  var delta = 100 - Math.random() * 50;
+  var delta = 200 - Math.random() * 100;
 
   if (this.isDeleting) { delta /= 2; }
 
